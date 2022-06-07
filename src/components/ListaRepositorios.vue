e22175b300a135281fb58c888553d163d2d784a8
andycodev <andycodev@gmail.com>
2022-06-07 08:46:22 -0500

    cambiosn en interface

--- src/components/ListaRepositorios.vue
+++ src/components/ListaRepositorios.vue
@@ -1,17 +1,16 @@
 <template>
-
   <!-- <img alt="Vue logo" src="src/assets/logo.png" /> -->
-
-  <h1>{{data.name}}</h1>
-  <span>{{data.login}}</span>
-  <h2>Mis repositorios</h2>
-  <ul>
-    <li v-for="repo in repos" :key="repo.id">
-      {{repo.name}}
-      <a :href="repo.html_url" target="_blank">Ir al repositorio</a>
-    </li>
-  </ul>
-
+  <div class="container">
+    <h1>{{ data.name }}</h1>
+    <span>{{ data.login }}</span>
+    <h2>Mis repositorios ({{ repos.length }})</h2>
+    <ul>
+      <li v-for="repo in repos" :key="repo.id">
+        {{ repo.name }}
+        <a :href="repo.html_url" target="_blank">Ir al repositorio</a>
+      </li>
+    </ul>
+  </div>
 </template>
 
 <script>
@@ -21,9 +20,8 @@ const API_REPOS = "https://api.github.com/users/andycodev/repos";
 export default {
   data() {
     return {
-      msg: "holaaaaa",
       data: {},
-      repos: []
+      repos: [],
     };
   },
   mounted() {
@@ -47,9 +45,18 @@ export default {
 </script>
 
 <style scoped>
+.container {
+  background: #242424;
+  color: azure;
+  width: 1000px;
+  margin: 0 auto;
+  padding: 15px;
+  border-radius: 1%;
+}
 
-img {
-  margin-left: 30px;
+h2 {
+  font-size: 30px;
+  text-decoration: dashed
 }
 
 ul li {
@@ -57,12 +64,20 @@ ul li {
   font-size: 40px;
   margin: 20px;
   list-style-type: none;
-  
 }
 
 ul li a {
- font-size: 10px;
- text-decoration: none;
+  font-size: 20px;
+  text-decoration: none;
+  /*  display: block; */
+  background: #42b883;
+  color: #ffffff;
+  border-radius: 2%;
+  padding: 5px;
 }
 
+ul li a:hover {
+  background: #ffffff;
+  color: #242424;
+}
 </style>
